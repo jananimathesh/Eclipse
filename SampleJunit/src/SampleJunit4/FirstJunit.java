5aba29b2cd1ec57bcc3f43b38e032af44e78579a
HI <HI@192.168.1.37>
2021-12-04 17:55:13 +0530

    Commiting all files

---
+++ SampleJunit/src/SampleJunit4/FirstJunit.java
@@ -0,0 +1,20 @@
+package SampleJunit4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FirstJunit {
+
+	@Test
+	void test() {
+		assertTrue(true);
+		//fail("Not yet implemented");
+	}
+	@Test
+	void test2() {
+		assertTrue(true);
+		//fail("Not yet implemented");
+	}
+	
+}
